9567fc7f6d6016c1c9d5383b0c7e62b7861e4039
Dylan <dylanleonardo11@gmail.com>
2025-03-05 23:54:39 -0500

    Iconos y No vacios

--- AppToDo/src/app/home/home.page.ts
+++ AppToDo/src/app/home/home.page.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { ITask } from '../Interfaces/ITask';
-import { AlertController } from '@ionic/angular';
-import { take } from 'rxjs';
+import { AlertController, IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -34,30 +33,60 @@ export class HomePage {
       buttons: [{
         text: 'Cancelar',
         role: 'cancel',
-        // handler: () => (
-        //   console.log('')
-        // )
       }, {
         text: 'Agregar',
         handler: (data) => {
-          this.tasks.push({
-            id: this.tasks.length + 1,
-            title: data.title,
-            done: false
-          })
+          if (data.title != ""){
+            this.tasks.push({
+              id: this.tasks.length + 1,
+              title: data.title,
+              done: false
+            })
+          } else {
+            alert.message = "El campo no puede estar vacio";
+            return false;
+          }
+          return true;
         }
       }]
     });
     await alert.present();
   }
 
-  toggleTaksDone(task: ITask){
+  toggleTaksDone(task: ITask, slidingItem: IonItemSliding){
     task.done = !task.done
+    slidingItem.close();
   }
 
-  deleteTask(task: ITask){
-    console.log('Borrando')
+  deleteTask(task: ITask) {
+    console.log(this.tasks.indexOf(task));
+    //Eliminar Dato (Metodo 1)
+    this.tasks.splice(this.tasks.indexOf(task), 1);
+    //Eliminar Dato(Metodo 2)
+    // this.tasks = this.tasks.filter(t => t.id !== task.id);
+  }
+
+  async updateTask(task: ITask) {
+    const alert = await this._AlertController.create({
+      header: "Editar Tarea",
+      inputs: [{
+        type: 'text',
+        name: 'title',
+        value: task.title,
+      }],
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel',
+      }, {
+        text: 'Actualizar',
+        handler: (data) => {
+          this.tasks [this.tasks.indexOf(task)].title = data.title;
+        }
+      }]
+    });
+    await alert.present();
   }
 }
 
 
+//51.47 decoracion
